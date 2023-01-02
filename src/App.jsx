5c11332f308850a3b30bab5014cd05e62edce788
Bruno Netto <mir_brosa@uolinc.com>
2023-01-02 19:03:45 -0300

    feat(componente header): aplicando e entendendo funcionamento do css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+
+import './styles.css'
 
 export function App() {
   return (
     <>
+
+     <Header />
       <Post 
         author="Bruno Netto" 
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam fugit tenetur obcaecati temporibus doloremque laudantium dignissimos ipsam, velit inventore officia natus veniam eveniet voluptatibus blanditiis aperiam voluptate cum sed quia!" 
