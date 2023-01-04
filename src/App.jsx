4ad1af458bf26882f510b7a32d95b773bf03e922
Bruno Netto <mir_brosa@uolinc.com>
2023-01-03 22:47:14 -0300

    feat(componente post): criando primeira versão do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
-import { Post } from './Post'
+import { Post } from './components/Post'
 
 import './global.css'
 import styles from './App.module.css'
@@ -14,15 +14,8 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post 
-            author="Bruno Netto" 
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam fugit tenetur obcaecati temporibus doloremque laudantium dignissimos ipsam, velit inventore officia natus veniam eveniet voluptatibus blanditiis aperiam voluptate cum sed quia!" 
-          />
-
-          <Post 
-            author="Diego Fernandes" 
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam fugit tenetur obcaecati temporibus doloremque laudantium dignissimos ipsam, velit inventore officia natus veniam eveniet voluptatibus blanditiis aperiam voluptate cum sed quia!" 
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </>
