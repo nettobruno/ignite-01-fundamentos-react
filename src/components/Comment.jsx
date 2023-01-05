66096666fee0bbb8d3d9f6a525d1311a254fe3ec
Bruno Netto <mir_brosa@uolinc.com>
2023-01-05 19:49:38 -0300

    feat(componente avatar): criando componente de avatar para reutilizar código

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
+import { Avatar } from './Avatar'
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/nettobruno.png" />
+      <Avatar hasBorder={false} avatarUrl="https://github.com/nettobruno.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css"
 
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img 
-          className={styles.avatar}
-          src="https://github.com/nettobruno.png" 
-        />
+       <Avatar avatarUrl="https://github.com/nettobruno.png" />
 
         <strong>Bruno Netto</strong>
         <span>Web Developer</span>
